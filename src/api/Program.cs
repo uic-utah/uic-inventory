836e64419b2b42ca5af5478f5093b806101164ff
steveoh <sgourley@utah.gov>
2021-04-12 09:25:56 -0600

    chore: add serilog console logging

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,17 +1,16 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace api {
   public class Program {
-    public static void Main(string[] args) {
-      CreateHostBuilder(args).Build().Run();
-    }
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => {
-              webBuilder.UseStartup<Startup>();
-            })
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    public static void Main(string[] args)
+      => CreateHostBuilder(args).Build().Run();
+    public static IHostBuilder CreateHostBuilder(string[] args)
+      => Host.CreateDefaultBuilder(args)
+          .UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration))
+          .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+          .UseServiceProviderFactory(new AutofacServiceProviderFactory());
   }
 }
