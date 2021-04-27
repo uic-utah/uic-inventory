160c8f28187f3c8f8d392b7801cd2607cbe9b0b1
steveoh <sgourley@utah.gov>
2021-04-27 14:36:53 -0600

    refactor(app): auto import react because you can

--- src/api/uic-inventory/vite.config.js
+++ src/api/uic-inventory/vite.config.js
@@ -4,6 +4,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  esbuild: {
+    jsxInject: `import * as React from 'react';`
+  },
   server: {
     proxy: {
       '/graphql': 'http://localhost:5000/graphql'
