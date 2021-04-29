372dc91b34ade64118c7a99fe1491984002c54e8
steveoh <sgourley@utah.gov>
2021-04-29 11:45:56 -0600

    refactor: switch to docker compose

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")/.."
 
 docker compose down
 echo "starting databases"
-docker compose up --detach db cache
+docker compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
 echo "starting api"
 dotnet watch --project ./src/api/api.csproj run
