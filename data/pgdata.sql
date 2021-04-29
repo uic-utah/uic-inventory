f997695446c36411dc0fd29594c00af5479deb72
steveoh <sgourley@utah.gov>
2021-04-29 10:58:23 -0600

    chore(db): fix invalid sql

--- data/pgdata.sql
+++ data/pgdata.sql
@@ -4,16 +4,16 @@ CREATE TYPE public.notification_types AS ENUM ('new_user_account_registration', 
 
 CREATE TABLE public.accounts (
   id serial NOT NULL,
-  utah_id character NOT NULL varying(128) UNIQUE,
-  first_name character NOT NULL varying(128),
-  last_name character NOT NULL varying(128),
-  organization character varying(512),
-  email character varying(512),
-  phone character varying(64),
-  mailing_address character varying(512),
-  city character varying(128),
-  state character varying(128),
-  zip_code character varying(64),
+  utah_id varchar(128) UNIQUE NOT NULL,
+  first_name varchar(128) NOT NULL,
+  last_name varchar(128) NOT NULL,
+  organization varchar(512),
+  email varchar(512),
+  phone varchar(64),
+  mailing_address varchar(512),
+  city varchar(128),
+  state varchar(128),
+  zip_code varchar(64),
   receive_notifications boolean NOT NULL DEFAULT FALSE,
   account_access access_level NOT NULL DEFAULT 'standard' :: access_level,
   complete_profile boolean GENERATED ALWAYS AS (CASE WHEN
@@ -34,11 +34,13 @@ DROP TABLE IF EXISTS public.notifications;
 CREATE TABLE public.notifications (
   id serial NOT NULL,
   notification_type notification_types NULL,
-  created_at TIMESTAMP NOT NULL DEFAFULT now(),
-  url character varying(512),
+  created_at TIMESTAMP NOT NULL,
+  url varchar(512),
   additional_data jsonb,
   CONSTRAINT notification_primary_key PRIMARY KEY (id)
 );
+
+ALTER TABLE public.notifications ALTER COLUMN created_at SET DEFAULT now();
 
 DROP TABLE IF EXISTS public.notification_receipts;
 
@@ -49,7 +51,7 @@ CREATE TABLE public.notification_receipts (
   recipient_id integer NULL,
   notification_fk integer NOT NULL,
   CONSTRAINT notification_receipt_primary_key PRIMARY KEY (id),
-  CONSTRAINT notification_receipt_fk FOREIGN KEY (notification_fk) REFERENCES public.notifications(id)
+  CONSTRAINT notification_receipt_fk FOREIGN KEY (notification_fk) REFERENCES public.notifications(id),
   CONSTRAINT accounts_fk FOREIGN KEY (recipient_id) REFERENCES public.accounts(id)
 );
 
