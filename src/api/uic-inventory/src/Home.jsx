60b1a31feb8ee8e75c7a8636d252f3b67a1101ea
steveoh <sgourley@utah.gov>
2021-04-29 18:32:18 -0600

    chore(app): use auth context and show different components

--- src/api/uic-inventory/src/Home.jsx
+++ src/api/uic-inventory/src/Home.jsx
@@ -1,6 +1,13 @@
 import { List } from 'react-content-loader';
+import { AuthContext } from './AuthProvider';
 
 export function Home() {
+  const { isAuthenticated } = React.useContext(AuthContext);
+
+  return isAuthenticated() ? <SitesAndInventory /> : <GenericLandingPage />;
+}
+
+function SitesAndInventory() {
   return (
     <main>
       <div className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -8,7 +15,22 @@ export function Home() {
           <div className="h-full p-4 border-4 border-gray-200 border-dashed rounded-lg">
             <h1 className="mb-3 text-2xl font-medium">Your sites and inventory</h1>
             <List animate={false} />
-            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function GenericLandingPage() {
+  return (
+    <main>
+      <div className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <div className="h-full p-4 border-4 border-gray-200 border-dashed rounded-lg">
+            <h1 className="mb-3 text-2xl font-medium">About the program</h1>
+            <List animate={false} />
+          </div>
         </div>
       </div>
     </main>
