3c37de38bae34630abb3b45839c0f22b67b78bde
steveoh <sgourley@utah.gov>
2021-04-29 18:26:18 -0600

    chore(app): use proxy for login/logout methods on api

--- src/api/uic-inventory/vite.config.js
+++ src/api/uic-inventory/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   esbuild: {
-    jsxInject: `import * as React from 'react';`
+    jsxInject: `import * as React from 'react';`,
   },
   server: {
     proxy: {
-      '/graphql': 'http://localhost:5000/graphql'
-    }
-  }
-})
+      '/graphql': 'http://localhost:5000/graphql',
+      '/api': {
+        target: 'https://localhost:5001/',
+        changeOrigin: true,
+        secure: false,
+        protocolRewrite: 'https',
+      },
+    },
+  },
+});
