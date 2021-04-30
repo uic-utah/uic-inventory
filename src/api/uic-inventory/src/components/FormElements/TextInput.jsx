3c45b39fc9a3c224adb0b8ddc488ce528ea357aa
steveoh <sgourley@utah.gov>
2021-04-29 18:32:48 -0600

    syle(app): :art: prettier

--- src/api/uic-inventory/src/components/FormElements/TextInput.jsx
+++ src/api/uic-inventory/src/components/FormElements/TextInput.jsx
@@ -1,5 +1,5 @@
 import ErrorMessageTag from './ErrorMessage';
-import { ErrorMessage } from '@hookform/error-message'
+import { ErrorMessage } from '@hookform/error-message';
 
 const camelToProper = (text) => {
   const parts = text.split(/(?=[A-Z])/);
@@ -9,19 +9,20 @@ const camelToProper = (text) => {
     return firstWord;
   }
 
-  const theRest = parts.slice(1).map(x => x.toLowerCase());
+  const theRest = parts.slice(1).map((x) => x.toLowerCase());
 
   return `${firstWord} ${theRest.join()}`;
-}
+};
 
 function TextInput({ register, errors, id, text, type }) {
-  return (<>
-    <label htmlFor={id} className="block font-medium text-gray-700">
-      {text || camelToProper(id)}
-    </label>
-    <input type={type || 'text'} id={id} {...register(id)} />
-    <ErrorMessage errors={errors} name={id} as={ErrorMessageTag} />
-  </>
+  return (
+    <>
+      <label htmlFor={id} className="block font-medium text-gray-700">
+        {text || camelToProper(id)}
+      </label>
+      <input type={type || 'text'} id={id} {...register(id)} />
+      <ErrorMessage errors={errors} name={id} as={ErrorMessageTag} />
+    </>
   );
 }
 
