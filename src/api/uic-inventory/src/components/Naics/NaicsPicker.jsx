e2bde7535c7d36ac8ce9742ef8903ae24db380c0
steveoh <sgourley@utah.gov>
2021-05-10 18:57:16 -0600

    wip(app): start naics component

---
+++ src/api/uic-inventory/src/components/Naics/NaicsPicker.jsx
@@ -0,0 +1,34 @@
+const topLevelItems = [
+  { code: 11, value: 'Agriculture, Forestry, Fishing and Hunting' },
+  { code: 21, value: 'Mining, Quarrying, and Oil and Gas Extraction' },
+  { code: 22, value: 'Utilities' },
+  { code: 23, value: 'Construction' },
+  { code: '31-33', value: 'Manufacturing' },
+  { code: 42, value: 'Wholesale Trade' },
+  { code: '44-45', value: 'Retail Trade' },
+  { code: '48-49', value: 'Transportation and Warehousing' },
+  { code: 51, value: 'Information' },
+  { code: 52, value: 'Finance and Insurance' },
+  { code: 53, value: 'Real Estate and Rental and Leasing' },
+  { code: 54, value: 'Professional, Scientific, and Technical Services' },
+  { code: 55, value: 'Management of Companies and Enterprises' },
+  { code: 56, value: 'Administrative and Support and Waste Management and Remediation Services' },
+  { code: 61, value: 'Educational Services' },
+  { code: 62, value: 'Health Care and Social Assistance' },
+  { code: 71, value: 'Arts, Entertainment, and Recreation' },
+  { code: 72, value: 'Accommodation and Food Services' },
+  { code: 81, value: 'Other Services (except Public Administration)' },
+  { code: 92, value: 'Public Administration' },
+];
+
+function NaicsPicker() {
+  return (
+    <div className="flex flex-wrap">
+      {topLevelItems.map((item) => (
+        <div className="border border-gray-600 rounded-md">{item.value}</div>
+      ))}
+    </div>
+  );
+}
+
+export default NaicsPicker;
