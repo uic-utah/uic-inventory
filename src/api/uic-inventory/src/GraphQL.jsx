e2ffaf391b3bb548da4c7f19d67a919845ba2a58
steveoh <sgourley@utah.gov>
2021-05-18 09:29:04 -0600

    refactor(app): move queries and schema to separate import

---
+++ src/api/uic-inventory/src/GraphQL.jsx
@@ -0,0 +1,76 @@
+export const AccountMutation = `mutation updateAccount($input: AccountInput!) {
+  updateAccount(input: $input) {
+    account {
+     id
+    }
+  }
+}`;
+
+export const AccountQuery = `query GetAccount($id: Int!) {
+  accountById(id: $id) {
+    firstName
+    lastName
+    email
+    organization
+    phoneNumber
+    mailingAddress
+    city
+    state
+    zipCode
+    receiveNotifications
+    access
+  }
+}`;
+
+export const ContactQuery = `query SiteById($id: Int!) {
+  siteById(id: $id) {
+    name
+    owner {
+      firstName
+      lastName
+      email
+      organization
+      phoneNumber
+      mailingAddress
+      city
+      state
+      zipCode
+    }
+    contacts {
+      firstName
+      lastName
+      email
+      organization
+      phoneNumber
+      mailingAddress
+      city
+      state
+      zipCode
+      contactType
+    }
+  }
+}`;
+
+export const SitesQuery = `query MySites {
+  mySites {
+    id
+    name
+    naicsTitle
+  }
+}`;
+
+export const SiteMutation = `mutation CreateSite($input: SiteInput!) {
+  createSite(input: $input) {
+    site {
+      id
+    }
+  }
+}`;
+
+export const ContactMutation = `mutation CreateContact($input: ContactInput!) {
+  createContact(input: $input) {
+    contact {
+      id
+    }
+  }
+}`;
