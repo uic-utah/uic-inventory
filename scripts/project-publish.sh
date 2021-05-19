b4ef83e5ad146fbd3dd102e864cb6d5e8a140b28
steveoh <sgourley@utah.gov>
2021-05-19 13:29:45 -0600

    chore(api): add clean to publish

--- scripts/project-publish.sh
+++ scripts/project-publish.sh
@@ -2,4 +2,8 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-dotnet publish ./src/api/api.csproj -c Release -r linux-x64
+echo 'cleaning'
+dotnet clean --nologo ./src/api/api.csproj --verbosity quiet
+
+echo 'creating publish files'
+dotnet publish ./src/api/api.csproj -c Release -r linux-x64 --no-self-contained
