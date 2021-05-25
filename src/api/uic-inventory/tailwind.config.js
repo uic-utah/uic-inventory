3a5751097996cec626e78c82a7f12b528cf80bdc
steveoh <sgourley@utah.gov>
2021-05-24 18:11:29 -0600

    chore(app): update tailwind to scan dartboard styles

--- src/api/uic-inventory/tailwind.config.js
+++ src/api/uic-inventory/tailwind.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{js,jsx}'],
+  purge: ['./index.html', './src/**/*.{js,jsx}', './node_modules/@agrc/dart-board/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     minHeight: {
-      'profile': '36em'
+      profile: '36em',
     },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
