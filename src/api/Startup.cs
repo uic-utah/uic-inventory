a575b84d439585ed457a3bde5c4b853e2c94a174
steveoh <sgourley@utah.gov>
2021-05-24 18:08:28 -0600

    style(api): remove unused using

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace api {
