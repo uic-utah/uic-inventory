cce6f1da69e541f1476a5fe73be34626d6549df4
steveoh <sgourley@utah.gov>
2021-05-26 10:02:57 -0600

    fix(app): use actual tailwind classes

--- src/api/uic-inventory/src/Routes.jsx
+++ src/api/uic-inventory/src/Routes.jsx
@@ -6,9 +6,9 @@ const contextClass = {
   success: 'bg-green-400',
   error: 'bg-red-600',
   info: 'bg-gray-600',
-  warning: 'bg-orange-400',
+  warning: 'bg-yellow-400 text-black',
   default: 'bg-indigo-600',
-  dark: 'bg-white-600 font-gray-300',
+  dark: 'bg-white-600 text-gray-300',
 };
 
 function Routes() {
