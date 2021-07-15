3b81924554a2a9f94e214a6840cebb48b72fe1d1
steveoh <sgourley@utah.gov>
2021-07-14 22:32:28 -0600

    chore(app): add eslint to vite build

--- src/api/uic-inventory/vite.config.js
+++ src/api/uic-inventory/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), reactJsx()],
+  plugins: [reactRefresh(), reactJsx(), eslintPlugin()],
   server: {
     proxy: {
       '/api': {
