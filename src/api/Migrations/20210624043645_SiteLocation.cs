dac00265f087fbd36c35c0bc3bd6ff5e53a2d62a
steveoh <sgourley@utah.gov>
2021-07-14 22:36:14 -0600

    style(api): format

--- src/api/Migrations/20210624043645_SiteLocation.cs
+++ src/api/Migrations/20210624043645_SiteLocation.cs
@@ -1,37 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace api.Migrations
-{
-    public partial class SiteLocation : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "address",
-                schema: "public",
-                table: "sites",
-                type: "text",
-                nullable: true);
+namespace api.Migrations {
+  public partial class SiteLocation : Migration {
+    protected override void Up(MigrationBuilder migrationBuilder) {
+      migrationBuilder.AddColumn<string>(
+          name: "address",
+          schema: "public",
+          table: "sites",
+          type: "text",
+          nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "geometry",
-                schema: "public",
-                table: "sites",
-                type: "jsonb",
-                nullable: true);
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "address",
-                schema: "public",
-                table: "sites");
-
-            migrationBuilder.DropColumn(
-                name: "geometry",
-                schema: "public",
-                table: "sites");
-        }
+      migrationBuilder.AddColumn<string>(
+          name: "geometry",
+          schema: "public",
+          table: "sites",
+          type: "jsonb",
+          nullable: true);
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder) {
+      migrationBuilder.DropColumn(
+          name: "address",
+          schema: "public",
+          table: "sites");
+
+      migrationBuilder.DropColumn(
+          name: "geometry",
+          schema: "public",
+          table: "sites");
+    }
+  }
 }
