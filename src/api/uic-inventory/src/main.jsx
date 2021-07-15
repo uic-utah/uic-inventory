73ee3362b7b03af797023b7d922641cae88c16e2
steveoh <sgourley@utah.gov>
2021-07-14 22:32:56 -0600

    chore(app): add react query provider

--- src/api/uic-inventory/src/main.jsx
+++ src/api/uic-inventory/src/main.jsx
@@ -1,11 +1,26 @@
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './styles/tailwind.css';
 import App from './App';
 import { StrictMode } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider
+    client={
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+    }
+  >
+    <StrictMode>
+      <App />
+      <ReactQueryDevtools initialIsOpen />
+    </StrictMode>
+  </QueryClientProvider>,
   document.getElementById('root')
 );
