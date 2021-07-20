0c9c0f8effd111b40aaff7411337f58a25299786
steveoh <sgourley@utah.gov>
2021-07-19 21:12:05 -0600

    refactor(app): use a reducer
    refs #49
    refs #46
    refs #31

--- src/api/uic-inventory/src/components/Pages/Sites/AddSiteLocation.jsx
+++ src/api/uic-inventory/src/components/Pages/Sites/AddSiteLocation.jsx
@@ -16,7 +16,7 @@ import Graphic from '@arcgis/core/Graphic';
 import Polygon from '@arcgis/core/geometry/Polygon';
 import Viewpoint from '@arcgis/core/Viewpoint';
 import { TailwindDartboard } from '../../Dartboard/Dartboard';
-import { useContext, useRef, useState, useEffect } from 'react';
+import { useContext, useEffect, useReducer, useRef } from 'react';
 import clsx from 'clsx';
 import { PinSymbol, PolygonSymbol } from '../../MapElements/MarkerSymbols';
 import { enablePolygonDrawing } from '../../MapElements/Drawing';
@@ -30,15 +30,10 @@ function AddSiteLocation() {
   const { siteId } = useParams();
   const history = useHistory();
   const mapDiv = useRef(null);
+  const isDirty = useRef(false);
   const pointAddressClickEvent = useRef(null);
   const parcelClickEvent = useRef(null);
   const siteDrawingEvents = useRef(null);
-  const [geocodeSuccess, setGeocodeSuccess] = useState(undefined);
-  const [address, setAddress] = useState(false);
-  const [siteGeometry, setSiteGeometry] = useState(false);
-  const [pointAddressClickEnabled, setPointAddressClickEnabled] = useState(false);
-  const [parcelClickEnabled, setParcelClickEnabled] = useState(false);
-  const [siteDrawingEnabled, setSiteDrawingEnabled] = useState(false);
   const { status, data } = useQuery(['site', siteId], () => ky.get(`/api/site/${siteId}`).json(), {
     enabled: siteId > 0,
   });
@@ -54,116 +49,188 @@ function AddSiteLocation() {
       return toast.error('We had some trouble adding the location');
     },
   });
-  const { formState, handleSubmit, setValue } = useForm({
+  const { handleSubmit, setValue } = useForm({
     resolver: yupResolver(schema),
   });
-
-  //* pull isDirty from form state to activate proxy
-  const { isDirty } = formState;
 
   const { mapView } = useWebMap(mapDiv, '80c26c2104694bbab7408a4db4ed3382');
   // zoom map on geocode
   const { setViewPoint } = useViewPointZooming(mapView);
   // manage graphics
-  const { setGraphic } = useGraphicManager(mapView);
+  const { setGraphic: setPolygonGraphic } = useGraphicManager(mapView);
+  const { setGraphic: setPointGraphic } = useGraphicManager(mapView);
 
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'initial-load': {
+        if (action.payload.geometry) {
+          setValue('geometry', action.payload.geometry);
+          state = { ...state, geometry: action.payload.geometry };
+
+          const shape = JSON.parse(action.payload.geometry);
+          const geometry = new Polygon({
+            type: 'polygon',
+            rings: shape.rings,
+            spatialReference: shape.spatialReference,
+          });
+
+          setPolygonGraphic(
+            new Graphic({
+              geometry: geometry,
+              attributes: {},
+              symbol: PolygonSymbol,
+            })
+          );
+
+          setViewPoint(new Viewpoint({ targetGeometry: geometry.centroid, scale: 1500 }));
+        }
+
+        if (action.payload.address) {
+          state = { ...state, address: action.payload.address };
+          setValue('address', action.payload.address);
+        }
+
+        if (state.address && state.geometry) {
+          state = { ...state };
+        }
+
+        isDirty.current = false;
+
+        return state;
+      }
+      case 'geocode-success': {
+        isDirty.current = true;
+        setValue('address', action.payload.attributes.InputAddress);
+        setPointGraphic(new Graphic(action.payload));
+        setViewPoint(new Viewpoint({ targetGeometry: action.payload.geometry, scale: 1500 }));
+
+        return {
+          ...state,
+          address: action.payload.attributes.InputAddress,
+          formStatus: 'allow-site-boundary-from-click',
+        };
+      }
+      case 'skip-geocoding': {
+        return { ...state, formStatus: 'allow-site-address-from-click' };
+      }
+      case 'activate-site-address-from-click': {
+        if (state.activeTool === 'site-address-click') {
+          return { ...state, activeTool: null };
+        }
+
+        return { ...state, activeTool: 'site-address-click' };
+      }
+      case 'address-clicked': {
+        const coordinates = `${Math.round(action.payload.geometry.x)}, ${Math.round(action.payload.geometry.y)}`;
+        setValue('address', coordinates);
+        isDirty.current = true;
+        setPointGraphic(action.payload);
+
+        if (mapView.current.scale > 10489.34) {
+          mapView.current.goTo(new Viewpoint({ targetGeometry: action.payload.geometry, scale: 10480 }));
+        }
+
+        return { ...state, address: coordinates, formStatus: 'allow-site-boundary-from-click' };
+      }
+      case 'select-site-from-parcel': {
+        if (state.activeTool === 'selecting-a-parcel') {
+          return { ...state, activeTool: null };
+        }
+
+        return { ...state, activeTool: 'selecting-a-parcel' };
+      }
+      case 'draw-site-boundary': {
+        if (state.activeTool === 'freehand-polygon-drawing') {
+          return { ...state, activeTool: null };
+        }
+
+        return { ...state, activeTool: 'freehand-polygon-drawing' };
+      }
+      case 'set-site-boundary': {
+        if (!action.payload) {
+          setValue('geometry', null);
+          setPolygonGraphic(null);
+
+          return { ...state, geometry: null };
+        }
+
+        const geometry = JSON.stringify(action.payload.geometry.toJSON());
+        setValue('geometry', geometry);
+        isDirty.current = true;
+
+        setPolygonGraphic(action.payload);
+
+        return {
+          ...state,
+          geometry: geometry,
+          activeTool: action.meta === 'freehand-polygon-drawing' ? null : state.activeTool,
+        };
+      }
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
+    address: undefined,
+    geometry: undefined,
+    activeTool: undefined,
+    formStatus: undefined,
+  });
+
+  // hydrate form with existing data
   useEffect(() => {
     if (status !== 'success') {
       return;
     }
 
-    if (data.geometry) {
-      setSiteGeometry(data.geometry);
-      setValue('geometry', data.geometry);
-
-      const shape = JSON.parse(data.geometry);
-      const geometry = new Polygon({
-        type: 'polygon',
-        rings: shape.rings,
-        spatialReference: shape.spatialReference,
-      });
-
-      setGraphic(
-        new Graphic({
-          geometry: geometry,
-          attributes: {},
-          symbol: PolygonSymbol,
-        })
-      );
-
-      setViewPoint(new Viewpoint({ targetGeometry: geometry.centroid, scale: 1500 }));
-    }
-
-    if (data.address) {
-      setAddress(data.address);
-      setValue('address', data.address);
-    }
-  }, [data, status, setGraphic, setViewPoint, setValue]);
+    dispatch({
+      type: 'initial-load',
+      payload: {
+        geometry: data?.geometry,
+        address: data?.address,
+      },
+    });
+  }, [data, status]);
 
   // activate point clicking for selecting an address
   useEffect(() => {
-    // if geocoding was successful then skip choosing the address by a point method
-    if (geocodeSuccess) {
-      return setPointAddressClickEnabled(false);
-    }
-
-    // if the event was disabled clear any existing events
-    if (!pointAddressClickEnabled) {
+    // if the tool was changed clear existing events
+    if (state.activeTool !== 'site-address-click') {
       pointAddressClickEvent.current?.remove();
       pointAddressClickEvent.current = null;
 
-      mapView.current.graphics.removeAll();
-
       return;
     }
-
-    setParcelClickEnabled(false);
-    setSiteDrawingEnabled(false);
 
     mapView.current.focus();
 
     // enable clicking on the map to set the address
     pointAddressClickEvent.current = mapView.current.on('immediate-click', (event) => {
-      setGraphic(
-        new Graphic({
-          geometry: event.mapPoint,
-          attributes: {},
-          symbol: PinSymbol,
-        })
-      );
+      const graphic = new Graphic({
+        geometry: event.mapPoint,
+        attributes: {},
+        symbol: PinSymbol,
+      });
 
-      setValue('address', `${Math.round(event.mapPoint.x)}, ${Math.round(event.mapPoint.y)}`);
-      setAddress(true);
-
-      if (mapView.current.scale > 10489.34) {
-        mapView.current.goTo(new Viewpoint({ targetGeometry: event.mapPoint, scale: 10480 }));
-      }
+      dispatch({ type: 'address-clicked', payload: graphic });
     });
 
     return () => {
       pointAddressClickEvent.current?.remove();
       pointAddressClickEvent.current = null;
     };
-  }, [geocodeSuccess, pointAddressClickEnabled, setValue, mapView, setGraphic]);
+  }, [state.activeTool]);
 
   // activate parcel hit test clicking
   useEffect(() => {
     // clean up events when disabled
-    if (!parcelClickEnabled) {
+    if (state.activeTool !== 'selecting-a-parcel') {
       parcelClickEvent.current?.remove();
       parcelClickEvent.current = null;
 
-      setGraphic(null);
-
-      setValue('geometry', null);
-      setSiteGeometry(false);
-
       return;
     }
-
-    // trigger other effects to clean up their events
-    setPointAddressClickEnabled(false);
-    setSiteDrawingEnabled(false);
 
     mapView.current.focus();
 
@@ -179,15 +246,13 @@ function AddSiteLocation() {
         })
         .then((test) => {
           if (test.results.length < 1) {
-            return setGraphic(null);
+            return dispatch({ type: 'set-site-boundary', payload: null, meta: 'selecting-a-parcel' });
           }
 
-          const parcelGraphic = test.results[0].graphic;
-          parcelGraphic.symbol = PolygonSymbol;
+          const graphic = test.results[0].graphic;
+          graphic.symbol = PolygonSymbol;
 
-          setGraphic(parcelGraphic);
-          setValue('geometry', JSON.stringify(parcelGraphic.geometry.toJSON()));
-          setSiteGeometry(true);
+          dispatch({ type: 'set-site-boundary', payload: graphic, meta: 'selecting-a-parcel' });
         });
     });
 
@@ -195,11 +260,11 @@ function AddSiteLocation() {
       parcelClickEvent.current?.remove();
       parcelClickEvent.current = null;
     };
-  }, [parcelClickEnabled, pointAddressClickEnabled, setValue, mapView, setGraphic]);
+  }, [state.activeTool]);
 
   // activate polygon site drawing
   useEffect(() => {
-    if (!siteDrawingEnabled || siteDrawingEnabled === 'complete') {
+    if (state.activeTool !== 'freehand-polygon-drawing') {
       for (let index = 0; index < siteDrawingEvents.current?.length; index++) {
         const event = siteDrawingEvents.current[index];
 
@@ -208,63 +273,38 @@ function AddSiteLocation() {
 
       siteDrawingEvents.current = null;
 
-      if (siteDrawingEnabled !== 'complete') {
-        mapView.current.graphics.removeAll();
-
-        setValue('geometry', null);
-        setSiteGeometry(false);
-      }
-
       return;
     }
 
-    setPointAddressClickEnabled(false);
-    setParcelClickEnabled(false);
-
-    const [drawAction, drawingEvent] = enablePolygonDrawing(mapView.current);
+    const [drawAction, drawingEvent] = enablePolygonDrawing(mapView.current, setPolygonGraphic);
 
     const finishEvent = drawAction.on(['draw-complete'], (event) => {
-      setSiteDrawingEnabled('complete');
-
-      setValue(
-        'geometry',
-        JSON.stringify(
-          new Polygon({
+      dispatch({
+        type: 'set-site-boundary',
+        payload: new Graphic({
+          geometry: new Polygon({
             type: 'polygon',
             rings: event.vertices,
             spatialReference: mapView.current.spatialReference,
-          }).toJSON()
-        )
-      );
-
-      setSiteGeometry(true);
+          }),
+          symbol: PolygonSymbol,
+        }),
+        meta: 'freehand-polygon-drawing',
+      });
     });
 
     siteDrawingEvents.current = [drawingEvent, finishEvent];
-  }, [siteDrawingEnabled, setValue, mapView]);
+  }, [state.activeTool]);
 
   const geocode = (result) => {
     if (!result) {
       return geocodeError('No match found');
     }
 
-    setGeocodeSuccess(true);
-    setValue('address', result.attributes.InputAddress);
-    setAddress(true);
-
-    setGraphic(new Graphic(result));
-    setViewPoint(new Viewpoint({ targetGeometry: result.geometry, scale: 1500 }));
+    dispatch({ type: 'geocode-success', payload: result });
   };
 
-  const geocodeError = () => {
-    setGraphic(null);
-
-    setGeocodeSuccess(false);
-    setViewPoint(null);
-
-    setValue('address', null);
-    setAddress(false);
-  };
+  const geocodeError = () => dispatch({ type: 'skip-geocoding', payload: false });
 
   const addSiteLocation = async (formData) => {
     if (!isDirty) {
@@ -289,14 +329,14 @@ function AddSiteLocation() {
           <GridHeading text="Site Location" subtext="Set the address and polygon for your site">
             <p className="mb-3">
               First, find your site location by it&apos;s address. If you don&apos;t have an address{' '}
-              <button type="primary" onClick={() => setGeocodeSuccess(false)}>
+              <button type="primary" onClick={() => dispatch({ type: 'skip-geocoding', payload: null })}>
                 skip
               </button>{' '}
               this step.
             </p>
             <div
               className={clsx('px-4 py-5 ml-4 border rounded transition hover:opacity-100', {
-                'opacity-25': address,
+                'opacity-25': state.formStatus === 'allow-site-address-from-click' || state.address,
               })}
             >
               <TailwindDartboard
@@ -306,27 +346,27 @@ function AddSiteLocation() {
                 format="esrijson"
               />
             </div>
-            {geocodeSuccess === undefined || geocodeSuccess === true ? null : (
+            {state.formStatus !== 'allow-site-address-from-click' ? null : (
               <>
                 <p className="my-3">
                   The site address was not found, select a point for the site instead with the tool below.
                 </p>
                 <div
                   className={clsx('px-4 py-5 ml-4 border flex justify-center rounded transition hover:opacity-100', {
-                    'opacity-25': parcelClickEnabled || siteDrawingEnabled,
+                    'opacity-25': state.formStatus !== 'allow-site-address-from-click',
                   })}
                 >
                   <button
                     type="button"
-                    className={clsx({ 'bg-blue-800': pointAddressClickEnabled })}
-                    onClick={() => setPointAddressClickEnabled(!pointAddressClickEnabled)}
+                    className={clsx({ 'bg-blue-800': state.activeTool === 'site-address-click' })}
+                    onClick={() => dispatch({ type: 'activate-site-address-from-click', payload: '' })}
                   >
                     <PointIcon classes="h-6 text-white fill-current" />
                   </button>
                 </div>
               </>
             )}
-            {geocodeSuccess || address ? (
+            {state.formStatus === 'allow-site-boundary-from-click' || state.address ? (
               <>
                 <p className="my-3">
                   Next, select a parcel as the site polygon or draw the site polygon if the parcel cannot be used.
@@ -334,15 +374,15 @@ function AddSiteLocation() {
                 <div className="flex justify-around px-4 py-5 ml-4 border rounded">
                   <button
                     type="button"
-                    className={clsx({ 'bg-blue-800': parcelClickEnabled })}
-                    onClick={() => setParcelClickEnabled(!parcelClickEnabled)}
+                    className={clsx({ 'bg-blue-800': state.activeTool === 'selecting-a-parcel' })}
+                    onClick={() => dispatch({ type: 'select-site-from-parcel', payload: '' })}
                   >
                     <SelectPolygonIcon classes="h-6 text-white fill-current" />
                   </button>
                   <button
                     type="button"
-                    className={clsx({ 'bg-blue-800': siteDrawingEnabled })}
-                    onClick={() => setSiteDrawingEnabled(!siteDrawingEnabled)}
+                    className={clsx({ 'bg-blue-800': state.activeTool === 'freehand-polygon-drawing' })}
+                    onClick={() => dispatch({ type: 'draw-site-boundary', payload: '' })}
                   >
                     <PolygonIcon classes="h-6 text-white fill-current" />
                   </button>
@@ -364,16 +404,16 @@ function AddSiteLocation() {
                         <div className="flex justify-around">
                           <div className="flex flex-col justify-items-center">
                             <Label id="address" />
-                            <OkNotToggle classes="h-12" status={address} />
+                            <OkNotToggle classes="h-12" status={state.address} />
                           </div>
                           <div className="flex flex-col justify-items-center">
                             <Label id="site" text="Site Location" />
-                            <OkNotToggle classes="h-12" status={siteGeometry} />
+                            <OkNotToggle classes="h-12" status={state.geometry} />
                           </div>
                         </div>
                       </div>
                       <div className="px-4 py-3 text-right bg-gray-100 sm:px-6">
-                        <button type="submit" disabled={!(address && siteGeometry)}>
+                        <button type="submit" disabled={!state.formStatus === 'complete'}>
                           Next
                         </button>
                       </div>
