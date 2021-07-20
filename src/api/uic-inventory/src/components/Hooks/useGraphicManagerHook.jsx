f30d3ee51fe0ca0e9abbc417f99021a39c00e78f
steveoh <sgourley@utah.gov>
2021-07-19 21:07:40 -0600

    feat(app): graphic hook can now manage specific graphics

--- src/api/uic-inventory/src/components/Hooks/useGraphicManagerHook.jsx
+++ src/api/uic-inventory/src/components/Hooks/useGraphicManagerHook.jsx
@@ -1,10 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export function useGraphicManager(mapView) {
-  const [graphic, setGraphic] = useState(null);
+  const [graphic, setGraphic] = useState();
+  const previousGraphic = useRef();
 
   useEffect(() => {
-    mapView.current.graphics.removeAll();
+    if (previousGraphic.current) {
+      if (Array.isArray(previousGraphic.current)) {
+        previousGraphic.current.forEach((x) => mapView.current.graphics.remove(x));
+      } else {
+        mapView.current.graphics.remove(previousGraphic.current);
+      }
+    }
+
+    if (graphic) {
+      previousGraphic.current = graphic;
+    }
+
+    if (Array.isArray(graphic)) {
+      return mapView.current.graphics.addMany(graphic);
+    }
+
     mapView.current.graphics.add(graphic);
   }, [graphic, mapView]);
 
