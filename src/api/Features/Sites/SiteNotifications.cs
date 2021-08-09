515b68b4f3c249c5f405606100e0215abf57c199
steveoh <sgourley@utah.gov>
2021-08-09 16:34:14 -0600

    chore(api): publish notification to update site status

--- src/api/Features/Sites/SiteNotifications.cs
+++ src/api/Features/Sites/SiteNotifications.cs
@@ -1,17 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using api.Infrastructure;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Features {
-  public class SiteNotifications {
-    public class SiteCreationNotification : INotification {
+  public static class SiteNotifications {
+    public class EditNotification : INotification {
+      public EditNotification(int siteId) {
+        SiteId = siteId;
+      }
 
+      public int SiteId { get; }
     }
 
-    public class SiteCreationNotificationHandler : INotificationHandler<SiteCreationNotification> {
-      public Task Handle(SiteCreationNotification notification, CancellationToken token) {
-        throw new NotImplementedException();
+    public class EditNotificationHandler : INotificationHandler<EditNotification> {
+      private readonly IAppDbContext _context;
+
+      public EditNotificationHandler(IAppDbContext context) {
+        _context = context;
+      }
+      private static bool GetContactStatus(ICollection<Contact> contacts) =>
+        contacts.Any(x => RequiredContactTypes.Types.Contains(x.ContactType));
+      private static bool GetLocationStatus(Site site) => site.Address is not null && site.Geometry is not null;
+      private static bool GetDetailStatus(Site site) =>
+        site.Name is not null &&
+        site.Ownership is not null &&
+        site.NaicsPrimary is not null &&
+        site.NaicsTitle is not null;
+      private static SiteStatus GetSiteStatus(Site site) =>
+        site.ContactStatus && site.DetailStatus && site.LocationStatus ?
+          SiteStatus.Complete :
+          SiteStatus.Incomplete;
+      public async Task Handle(EditNotification notification, CancellationToken token) {
+        var site = await _context.Sites
+          .Include(x => x.Contacts)
+          .SingleOrDefaultAsync(s => s.Id == notification.SiteId, token);
+
+        site.LocationStatus = GetLocationStatus(site);
+        site.DetailStatus = GetDetailStatus(site);
+        site.ContactStatus = GetContactStatus(site.Contacts);
+
+        site.Status = GetSiteStatus(site);
+
+        await _context.SaveChangesAsync(token);
       }
     }
   }
