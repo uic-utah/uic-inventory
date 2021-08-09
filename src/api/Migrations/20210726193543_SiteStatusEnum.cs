f5a4ba63b9e8445d6e89a0932fb0159ada9bbdbb
steveoh <sgourley@utah.gov>
2021-08-09 16:33:33 -0600

    chore(db): create site status fields

--- src/api/Migrations/20210726193543_SiteStatusEnum.cs
+++ src/api/Migrations/20210726193543_SiteStatusEnum.cs
@@ -1,23 +1,44 @@
-﻿using api.GraphQL;
+using api.Features;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace api.Migrations
-{
-    public partial class SiteStatusEnum : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterDatabase()
-                .Annotation("Npgsql:Enum:site_status", "incomplete,complete,submitted,authorized,ingested");
+namespace api.Migrations {
+  public partial class SiteStatusEnum : Migration {
+    protected override void Up(MigrationBuilder migrationBuilder) {
+      migrationBuilder.AlterDatabase()
+          .Annotation("Npgsql:Enum:site_status", "incomplete,complete,submitted,authorized,ingested");
 
-            migrationBuilder.AddColumn<SiteStatus>(
-                name: "status",
-                schema: "public",
-                table: "sites",
-                type: "site_status",
-                nullable: false,
-                defaultValue: SiteStatus.Incomplete);
-        }
+      migrationBuilder.AddColumn<SiteStatus>(
+          name: "status",
+          schema: "public",
+          table: "sites",
+          type: "site_status",
+          nullable: false,
+          defaultValue: SiteStatus.Incomplete);
+
+      migrationBuilder.AddColumn<bool>(
+          name: "detail_status",
+          schema: "public",
+          table: "sites",
+          type: "bool",
+          nullable: false,
+          defaultValue: false);
+
+      migrationBuilder.AddColumn<bool>(
+          name: "contact_status",
+          schema: "public",
+          table: "sites",
+          type: "bool",
+          nullable: false,
+          defaultValue: false);
+
+      migrationBuilder.AddColumn<bool>(
+          name: "location_status",
+          schema: "public",
+          table: "sites",
+          type: "bool",
+          nullable: false,
+          defaultValue: false);
+    }
 
     protected override void Down(MigrationBuilder migrationBuilder) {
       migrationBuilder.DropColumn(
@@ -25,8 +46,23 @@ namespace api.Migrations
         table: "sites",
         schema: "public");
 
+      migrationBuilder.DropColumn(
+        name: "detail_status",
+        table: "sites",
+        schema: "public");
+
+      migrationBuilder.DropColumn(
+        name: "contact_status",
+        table: "sites",
+        schema: "public");
+
+      migrationBuilder.DropColumn(
+        name: "location_status",
+        table: "sites",
+        schema: "public");
+
       migrationBuilder.AlterDatabase()
-                .OldAnnotation("Npgsql:Enum:site_status", "incomplete,complete,submitted,authorized,ingested");
+          .OldAnnotation("Npgsql:Enum:site_status", "incomplete,complete,submitted,authorized,ingested");
     }
   }
 }
