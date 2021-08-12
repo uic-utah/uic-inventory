757dc94d7391fe4b097e68eb6e3deac06861f606
steveoh <sgourley@utah.gov>
2021-08-11 19:09:57 -0600

    feat(app): show ui to delete contacts

--- src/api/uic-inventory/src/components/Pages/Sites/AddSiteContacts.jsx
+++ src/api/uic-inventory/src/components/Pages/Sites/AddSiteContacts.jsx
@@ -1,6 +1,14 @@
-import { BulletList } from 'react-content-loader';
+import { Fragment, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 import { useForm } from 'react-hook-form';
+import clsx from 'clsx';
+import { BulletList } from 'react-content-loader';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { TrashIcon } from '@heroicons/react/outline';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import ky from 'ky';
+import { useTable } from 'react-table';
+import { AuthContext } from '../../../AuthProvider';
 import { Chrome, onRequestError, toast, useParams, Link } from '../../PageElements';
 import {
   ContactSchema as schema,
@@ -14,9 +22,7 @@ import {
   SelectInput,
   TextInput,
 } from '../../FormElements';
-import { useEffect, useState } from 'react';
-import { useQuery, useMutation, useQueryClient } from 'react-query';
-import ky from 'ky';
+import { useOpenClosed } from '../../Hooks/useOpenClosedHook';
 
 const contactType = [
   {
@@ -76,29 +82,31 @@ const valueToLabel = (value) => {
 };
 
 function AddSiteContacts() {
+  const { siteId } = useParams();
+  const { authInfo } = useContext(AuthContext);
   const { control, formState, handleSubmit, register, reset, unregister } = useForm({
     resolver: yupResolver(schema),
   });
-  const { siteId } = useParams();
   const queryClient = useQueryClient();
   const { status, error, data } = useQuery(['contacts', siteId], () => ky.get(`/api/site/${siteId}/contacts`).json(), {
     enabled: siteId ?? 0 > 0 ? true : false,
     onError: (error) => onRequestError(error, 'We had some trouble finding your contacts.'),
   });
-  const { mutate } = useMutation((input) => ky.post('/api/contact', { json: { ...input, id: siteId } }), {
+  const { mutate } = useMutation((json) => ky.post('/api/contact', { json }), {
     onMutate: async (contact) => {
       await queryClient.cancelQueries(['contacts', siteId]);
       const previousValue = queryClient.getQueryData(['contacts', siteId]);
 
       queryClient.setQueryData(['contacts', siteId], (old) => ({
         ...old,
-        contacts: [...old.contacts, { ...contact, id: 9999, contactType: valueToLabel(contact.contactType) }],
+        contacts: [...old.contacts, { ...contact, contactType: valueToLabel(contact.contactType) }],
       }));
 
       return previousValue;
     },
     onSuccess: () => {
       toast.success('Contact created successfully!');
+
       reset({});
     },
     onSettled: () => {
@@ -116,25 +124,37 @@ function AddSiteContacts() {
 
   // set default fields to owner
   useEffect(() => {
-    if (data) {
-      let defaults = data.owner;
-
-      //! contact already added do not pre-fill
-      if (data.contacts?.find((x) => x.email === defaults.email)) {
-        return;
-      }
-
-      for (let name in defaults) {
-        if (Object.prototype.hasOwnProperty.call(defaults, 'name') && defaults[name] === null) {
-          defaults[name] = '';
-        }
-      }
-
-      defaults = { ...defaults, contactType: undefined };
-      console.log(defaults);
-
-      reset(defaults);
+    if (!data) {
+      return;
     }
+
+    const skipFields = ['access', 'receiveNotifications', 'errors', 'id'];
+    const owner = data.owner;
+
+    //! contact already added do not pre-fill
+    if (data.contacts?.find((x) => x.email === owner.email)) {
+      return;
+    }
+
+    const defaults = Object.keys(owner).reduce((object, key) => {
+      if (skipFields.includes(key)) {
+        return object;
+      }
+
+      if (owner[key] === null) {
+        object[key] = '';
+
+        return object;
+      }
+
+      object[key] = owner[key];
+
+      return object;
+    }, {});
+
+    console.log(defaults);
+
+    reset({ ...defaults, contactType: undefined });
   }, [data, reset]);
 
   // handle conditional control registration
@@ -146,20 +166,21 @@ function AddSiteContacts() {
     }
   }, [optional, register, unregister]);
 
-  const create = async (formData) => {
+  const create = (formData) => {
     if (!isDirty) {
       return toast.info("We've got your most current information");
     }
 
     const input = {
-      id: parseInt(siteId),
+      siteId: parseInt(siteId),
+      accountId: parseInt(authInfo.id),
     };
 
     for (let key of Object.keys(formData)) {
       input[key] = formData[key];
     }
 
-    await mutate({ ...input });
+    mutate(input);
   };
 
   return (
@@ -171,14 +192,20 @@ function AddSiteContacts() {
         <div className="min-h-screen mt-5 md:mt-0 md:col-span-2">
           {status === 'loading' && <BulletList style={{ height: '20em' }} />}
           {status !== 'loading' && !error && (
-            <>
-              <ContactsTable data={data?.contacts} />
-              <div className="px-4 py-3 text-right bg-gray-100 sm:px-6">
-                <Link type="button" to={`/site/${siteId}/add-location`}>
-                  Next
-                </Link>
+            <div className="flex flex-col">
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                  <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+                    <ContactTable data={data?.contacts} />
+                    <div className="px-4 py-3 text-right bg-gray-100 sm:px-6">
+                      <Link type="button" to={`/site/${siteId}/add-location`}>
+                        Next
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </>
+            </div>
           )}
           {error && (
             <h1>Something went terribly wrong</h1>
@@ -259,83 +286,235 @@ function AddSiteContacts() {
   );
 }
 
-function ContactsTable({ data }) {
-  return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div className="overflow-hidden border border-b-0 border-gray-200 shadow sm:rounded-t-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                  >
-                    Contact
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                  >
-                    Organization
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                  >
-                    Address
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {data?.length > 0 ? (
-                  data.map((item) => (
-                    <tr key={item.id}>
-                      <td className="px-3 py-4 whitespace-nowrap">
-                        <div className="text-sm font-medium text-gray-900">{`${item.firstName} ${item.lastName}`}</div>
-                        <div className="text-sm text-gray-500">{valueToLabel(item.contactType)}</div>
-                      </td>
-                      <td className="px-3 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{item.email}</div>
-                        <div className="text-sm text-gray-500">{item.phoneNumber}</div>
-                      </td>
-                      <td className="px-3 py-4">
-                        <div className="text-sm text-gray-900">{item.organization}</div>
-                      </td>
-                      <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
-                        <div>{item.mailingAddress}</div>
-                        <div>{`${item.city} ${item.state} ${item.zipCode}`} </div>
-                      </td>
-                      <td className="px-3 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <span className="text-indigo-600 hover:text-indigo-900">Edit</span>
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="5" className="px-3 py-4">
-                      <div className="text-sm text-center text-gray-900">
-                        Get started by filling out the form below to add your first site contact.
-                      </div>
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
+function ContactTable({ data }) {
+  const { siteId } = useParams();
+  const { authInfo } = useContext(AuthContext);
+  const [isOpen, { open, close }] = useOpenClosed();
+  const deleteContact = useRef();
+
+  const { mutate } = useMutation((json) => ky.delete(`/api/contact`, { json }), {
+    onMutate: async (mutationData) => {
+      await queryClient.cancelQueries(['contacts', siteId]);
+      const previousValue = queryClient.getQueryData(['contacts', siteId]);
+
+      queryClient.setQueryData(['contacts', siteId], (old) => {
+        return {
+          ...old,
+          contacts: old.contacts.filter((x) => x.id !== mutationData.contactId),
+        };
+      });
+
+      close();
+
+      return previousValue;
+    },
+    onSuccess: () => {
+      toast.success('This contact was removed.');
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['contacts', siteId]);
+    },
+    onError: (error, variables, previousValue) => {
+      queryClient.setQueryData(['contacts', siteId], previousValue);
+      onRequestError(error, 'We had some trouble deleting this contact.');
+    },
+  });
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Id',
+        accessor: 'id',
+      },
+      {
+        id: 'name',
+        Header: 'Name',
+        Cell: function name(data) {
+          return (
+            <>
+              <div className="text-sm font-medium text-gray-900">{`${data.row.original.firstName} ${data.row.original.lastName}`}</div>
+              <div className="text-sm text-gray-500">{valueToLabel(data.row.original.contactType)}</div>
+            </>
+          );
+        },
+      },
+      {
+        id: 'contact',
+        Header: 'Contact',
+        Cell: function contact(data) {
+          return (
+            <>
+              <div className="text-sm text-gray-900">{data.row.original.email}</div>
+              <div className="text-sm text-gray-500">{data.row.original.phoneNumber}</div>
+            </>
+          );
+        },
+      },
+      {
+        Header: 'Organization',
+        accessor: 'organization',
+      },
+      {
+        id: 'address',
+        Header: 'Address',
+        Cell: function contact(data) {
+          return (
+            <>
+              <div>{data.row.original.mailingAddress}</div>
+              <div>{`${data.row.original.city}, ${data.row.original.state} ${data.row.original.zipCode}`} </div>
+            </>
+          );
+        },
+      },
+      {
+        id: 'action',
+        Header: 'Action',
+        Cell: function action(data) {
+          return (
+            <TrashIcon
+              aria-label="delete site"
+              className="w-6 h-6 ml-1 text-red-600 cursor-pointer hover:text-red-900"
+              onClick={() => {
+                open();
+                deleteContact.current = data.row.original.id;
+              }}
+            />
+          );
+        },
+      },
+    ],
+    [open]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+  const queryClient = useQueryClient();
+
+  const remove = () =>
+    mutate({
+      siteId: parseInt(siteId),
+      accountId: parseInt(authInfo.id),
+      contactId: deleteContact.current,
+    });
+
+  return data?.length < 1 ? (
+    <div className="flex flex-col items-center">
+      <div className="px-5 py-4 m-6">
+        <h2 className="mb-1 text-xl font-medium">Create your first contact</h2>
+        <p className="text-gray-700">Get started by filling out the form below to add your first site contact.</p>
+        <div className="mb-6 text-sm text-center text-gray-900"></div>
       </div>
     </div>
+  ) : (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          open={isOpen}
+          onClose={() => {
+            close();
+            deleteContact.current = null;
+          }}
+          className="fixed inset-0 z-10 overflow-y-auto"
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+            </Transition.Child>
+
+            <span className="inline-block h-screen align-middle" aria-hidden="true">
+              &#8203;
+            </span>
+
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 mx-auto my-48 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <Dialog.Title className="text-lg font-medium leading-6 text-gray-900">
+                  Contact Deletion Confirmation
+                </Dialog.Title>
+                <Dialog.Description className="mt-1">This contact will be permanently deleted</Dialog.Description>
+
+                <p className="mt-1 text-sm text-gray-500">
+                  Are you sure you want to delete this contact? This action cannot be undone.
+                </p>
+
+                <div className="flex justify-around mt-6">
+                  <button type="button" className="bg-indigo-900" onClick={remove}>
+                    Yes
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      close();
+                      deleteContact.current = null;
+                    }}
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <table {...getTableProps()} className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          {headerGroups.map((headerGroup) => (
+            <tr key={headerGroup.index} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  key={`${headerGroup.index}-${column.id}`}
+                  {...column.getHeaderProps()}
+                  className="px-3 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()} className="bg-white divide-y divide-gray-200">
+          {rows.map((row) => {
+            prepareRow(row);
+
+            return (
+              <tr key={`${row.index}`} {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td
+                    key={`${row.index}-${cell.column.id}`}
+                    className={clsx(
+                      {
+                        'font-medium': ['action', 'id'].includes(cell.column.id),
+                        'text-right whitespace-nowrap': cell.column.id === 'action',
+                      },
+                      'px-3 py-4 whitespace-nowrap'
+                    )}
+                    {...cell.getCellProps()}
+                  >
+                    <div className="text-sm text-gray-900">{cell.render('Cell')}</div>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
