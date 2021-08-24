0cfcdabab905b577e93edd95738552081c3a442b
steveoh <sgourley@utah.gov>
2021-08-24 10:56:44 -0600

    refactor(app): show incomplete warning when adding wells

---
+++ src/api/uic-inventory/src/components/PageElements/IncompleteSiteWarning.jsx
@@ -0,0 +1,19 @@
+export function IncompleteSiteWarning() {
+  return (
+    <section>
+      <h2 className="mb-6 text-xl font-semibold text-center">
+        The site you are working on is incomplete. You must complete the site before adding wells.
+      </h2>
+      <h3 className="mb-2 text-lg font-medium">Site completeness includes</h3>
+      <ul className="ml-8 list-decimal">
+        <li>The site name, land ownership, and NAICS information must be entered and</li>
+        <li>The site address and location must be entered and</li>
+        <li>
+          At least one of the contacts listed must be the owner, owner/operator, or legal representative of the site.
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default IncompleteSiteWarning;
--- src/api/uic-inventory/src/components/PageElements/index.jsx
+++ src/api/uic-inventory/src/components/PageElements/index.jsx
@@ -2,10 +2,11 @@ import Chrome from './Chrome';
 import Navigation from './Navigation';
 import Header from './Header';
 import onRequestError from './ToastErrors';
+import IncompleteSiteWarning from './IncompleteSiteWarning';
 
 export { ToastContainer } from 'react-toastify';
 export { Link, Route, BrowserRouter as Router, Switch, useParams, useHistory } from 'react-router-dom';
 export { toast } from 'react-toastify';
 export * from './Icons';
 
-export { Chrome, Header, Navigation, onRequestError };
+export { Chrome, Header, Navigation, onRequestError, IncompleteSiteWarning };
