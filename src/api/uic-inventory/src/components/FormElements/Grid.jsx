a4c09fba3145eca4eb9247d27397722ee283f6e2
steveoh <sgourley@utah.gov>
2021-08-24 10:55:24 -0600

    feat(app): show project information
    refs #19

--- src/api/uic-inventory/src/components/FormElements/Grid.jsx
+++ src/api/uic-inventory/src/components/FormElements/Grid.jsx
@@ -4,10 +4,10 @@ export function FormGrid({ children }) {
   return <div className="grid grid-cols-6 gap-6">{children}</div>;
 }
 
-export function PageGrid({ children, heading, subtext, submit = false, disabled = false, submitLabel = 'Save' }) {
+export function PageGrid({ children, heading, subtext, site, submit = false, disabled = false, submitLabel = 'Save' }) {
   return (
     <div className="md:grid md:grid-cols-3 md:gap-6">
-      <GridHeading text={heading} subtext={subtext} />
+      <GridHeading text={heading} subtext={subtext} site={site} />
       <div className="mt-5 md:mt-0 md:col-span-2">
         <div className="overflow-hidden shadow sm:rounded-md">
           <div className="px-4 py-5 bg-white sm:p-6">{children}</div>
