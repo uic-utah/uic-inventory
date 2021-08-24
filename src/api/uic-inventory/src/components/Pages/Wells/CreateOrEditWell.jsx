ba182815517729577792238d8457cd4a2be63988
steveoh <sgourley@utah.gov>
2021-08-24 10:56:44 -0600

    refactor(app): show incomplete warning when adding wells

--- src/api/uic-inventory/src/components/Pages/Wells/CreateOrEditWell.jsx
+++ src/api/uic-inventory/src/components/Pages/Wells/CreateOrEditWell.jsx
@@ -122,21 +122,8 @@ function CreateOrEditWell() {
 
   return (
     <Chrome loading={status === 'loading'}>
-      {data?.status !== 'complete' ? (
-        <section>
-          <h2 className="mb-6 text-xl font-semibold text-center">
-            The site you are working on is incomplete. You must complete the site before adding wells.
-          </h2>
-          <h3 className="mb-2 text-lg font-medium">Site completeness includes</h3>
-          <ul className="ml-8 list-decimal">
-            <li>The site name, land ownership, and NAICS information must be entered and</li>
-            <li>The site address and location must be entered and</li>
-            <li>
-              At least one of the contacts listed must be the owner, owner/operator, or legal representative of the
-              site.
-            </li>
-          </ul>
-        </section>
+      {data?.siteStatus !== 'complete' ? (
+        <IncompleteSiteWarning />
       ) : (
         <>
           <form onSubmit={handleSubmit((data) => create(data))}>
