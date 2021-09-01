900f3e2e9e1e2ab7378be841f87ecd1565d8ae6a
steveoh <sgourley@utah.gov>
2021-09-01 17:30:30 -0600

    refactor(app): use js extension for items without jsx

--- src/api/uic-inventory/src/components/Pages/index.js
+++ src/api/uic-inventory/src/components/Pages/index.js
@@ -1,15 +1,15 @@
 import Profile from './Profile';
 import ContactProgram from './ContactProgram';
-import CreateOrEditWell from './Wells/CreateOrEditWell';
-import AddWellLocation from './Wells/AddWellLocation';
+
+import CreateOrEditInventory from './Wells/CreateOrEditInventory';
+import AddWells from './Wells/AddWells';
 
 import CreateOrEditSite from './Sites/CreateOrEditSite';
 import AddSiteContacts from './Sites/AddSiteContacts';
 import AddSiteLocation from './Sites/AddSiteLocation';
 
-export { SitesAndInventory, GenericLandingPage } from './Home';
-
 const Sites = { CreateOrEditSite, AddSiteContacts, AddSiteLocation };
-const Wells = { CreateOrEditWell, AddWellLocation };
+const Wells = { CreateOrEditInventory, AddWells };
 
+export { SitesAndInventory, GenericLandingPage } from './Home';
 export { ContactProgram, Profile, Sites, Wells };
