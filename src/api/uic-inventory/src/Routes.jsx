6e9a54a565a89205e01ef9ac99f6ca31616aa6bd
steveoh <sgourley@utah.gov>
2021-09-01 17:33:34 -0600

    refactor(app): update toast styling for v8

--- src/api/uic-inventory/src/Routes.jsx
+++ src/api/uic-inventory/src/Routes.jsx
@@ -3,15 +3,6 @@ import { ContactProgram, Profile, Sites, GenericLandingPage, SitesAndInventory, 
 import { AuthContext } from './AuthProvider';
 import { useContext } from 'react';
 
-const contextClass = {
-  success: 'bg-green-400',
-  error: 'bg-red-600',
-  info: 'bg-gray-600',
-  warning: 'bg-yellow-400 text-black',
-  default: 'bg-indigo-600',
-  dark: 'bg-white-600 text-gray-300',
-};
-
 function Routes() {
   const { isAuthenticated, completeProfile } = useContext(AuthContext);
 
@@ -21,13 +12,7 @@ function Routes() {
       <Switch>
         {isAuthenticated() ? <AuthenticatedRoutes completeProfile={completeProfile} /> : <UnauthenticatedRoutes />}
       </Switch>
-      <ToastContainer
-        toastClassName={({ type }) =>
-          contextClass[type || 'default'] +
-          ' my-3 relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer'
-        }
-        bodyClassName={() => 'text-sm font-white font-med block p-3'}
-      />
+      <ToastContainer theme="dark" />
     </Router>
   );
 }
