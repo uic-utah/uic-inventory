d0ed8cad0a85575933299823feafa8c586adfbed
steveoh <sgourley@utah.gov>
2021-09-28 19:38:28 -0600

    chore(api): add code to upload to bucket

--- src/api/Features/Wells/WellCommands.cs
+++ src/api/Features/Wells/WellCommands.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using api.Infrastructure;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -75,6 +79,7 @@ namespace api.Features {
     }
 
     public class Handler : IRequestHandler<Command, Well> {
+      private readonly string [] _acceptableFileTypes = new[] { "pdf", "png", "doc", "docx", "jpg", "jpeg" };
       private readonly IAppDbContext _context;
       private readonly IPublisher _publisher;
       private readonly ILogger _log;
@@ -95,15 +100,78 @@ namespace api.Features {
           .ForContext("verb", "PUT")
           .Debug("/api/well");
 
+        var errors = new List<string>(2);
+
         var wells = await _context.Wells
           .Where(x =>  request.Wells.SelectedWells.Contains(x.Id))
           .ToListAsync(cancellationToken);
 
+        if (request.Wells.ConstructionDetailsFile != null || request.Wells.InjectateCharacterizationFile != null) {
+          using var stream = new FileStream("./secrets/storage-service-account-dev.json", FileMode.Open, FileAccess.Read);
+          var client = await StorageClient.CreateAsync(GoogleCredential.FromStream(stream));
+
+          if (request.Wells.ConstructionDetailsFile != null) {
+            var fileType = request.Wells.ConstructionDetailsFile.FileName.Split('.').Last();
+
+            if (_acceptableFileTypes.Contains(fileType)) {
+              try {
+                var constructionFile = $"site_{request.Wells.SiteId}_inventory_{request.Wells.InventoryId}_user_{request.Wells.AccountId}_construction.{fileType}";
+                await client.UploadObjectAsync("ut-dts-agrc-uic-inventory-dev-documents",
+                  constructionFile,
+                  request.Wells.ConstructionDetailsFile.ContentType,
+                  request.Wells.ConstructionDetailsFile.OpenReadStream(), cancellationToken: cancellationToken);
+
+                request.Wells.ConstructionDetails = $"file::{constructionFile}";
+              } catch (Exception e) {
+                const string message = "Error uploading construction file";
+
+                _log.ForContext("site", request.Wells.SiteId)
+                    .ForContext("inventory", request.Wells.InventoryId)
+                    .ForContext("account", request.Wells.AccountId)
+                    .ForContext("fileType", fileType)
+                    .Error(e, message);
+
+                errors.Add(message);
+              }
+            }
+          }
+
+          if (request.Wells.InjectateCharacterizationFile != null) {
+            var fileType = request.Wells.InjectateCharacterizationFile.FileName.Split('.').Last();
+
+            if (_acceptableFileTypes.Contains(fileType)) {
+              try {
+                var injectateFile = $"site_{request.Wells.SiteId}_inventory_{request.Wells.InventoryId}_user_{request.Wells.AccountId}_injectate.{fileType}";
+                await client.UploadObjectAsync("ut-dts-agrc-uic-inventory-dev-documents",
+                  injectateFile,
+                  request.Wells.InjectateCharacterizationFile.ContentType,
+                  request.Wells.InjectateCharacterizationFile.OpenReadStream(), cancellationToken: cancellationToken);
+
+                request.Wells.InjectateCharacterization = $"file::{injectateFile}";
+              } catch (Exception e) {
+                const string message = "Error uploading injectate file";
+
+                _log.ForContext("site", request.Wells.SiteId)
+                    .ForContext("inventory", request.Wells.InventoryId)
+                    .ForContext("account", request.Wells.AccountId)
+                    .ForContext("fileType", fileType)
+                    .Error(e, message);
+
+                errors.Add(message);
+              }
+            }
+          }
+        }
+
         foreach (var well in wells) {
-            request.Wells.Update(well);
+          request.Wells.Update(well);
         }
 
         await _context.SaveChangesAsync(cancellationToken);
+
+        if (errors.Count > 0){
+          throw new Exception(string.Join("\n", errors));
+        }
 
         // await _publisher.Publish(new SiteNotifications.EditNotification(site.Id), cancellationToken);
 
