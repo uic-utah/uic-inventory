7e21755574338ef2439e9d9ffc5b3ebc482a601e
steveoh <sgourley@utah.gov>
2021-10-19 12:00:41 -0600

    chore(api): start frontend with watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -4,6 +4,9 @@ cd "$(dirname "$0")/.."
 docker compose down
 echo "starting databases"
 docker compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
+echo "starting front end"
+cd src/api/uic-inventory
+npm run dev &
 echo "starting api"
-cd src/api
+cd ..
 dotnet watch
