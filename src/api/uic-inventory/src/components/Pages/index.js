e1d6f44d68c05b320f4ff8dad2ab18c04c68ab7d
steveoh <sgourley@utah.gov>
2021-10-18 18:44:59 -0600

    feat(app): add ser contact screen
    refs #59

--- src/api/uic-inventory/src/components/Pages/index.js
+++ src/api/uic-inventory/src/components/Pages/index.js
@@ -4,13 +4,14 @@ import ContactProgram from './ContactProgram';
 import CreateOrEditInventory from './Wells/CreateOrEditInventory';
 import AddWells from './Wells/AddWells';
 import AddWellDetails from './Wells/AddWellDetails';
+import AddSerWellContact from './Wells/AddSerWellContact';
 
 import CreateOrEditSite from './Sites/CreateOrEditSite';
 import AddSiteContacts from './Sites/AddSiteContacts';
 import AddSiteLocation from './Sites/AddSiteLocation';
 
 const Sites = { CreateOrEditSite, AddSiteContacts, AddSiteLocation };
-const Wells = { CreateOrEditInventory, AddWells, AddWellDetails };
+const Wells = { CreateOrEditInventory, AddWells, AddWellDetails, AddSerWellContact };
 
 export { SitesAndInventory, GenericLandingPage } from './Home';
 export { ContactProgram, Profile, Sites, Wells };
