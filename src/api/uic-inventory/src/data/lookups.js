3767f08dee65d5457b8db6952e20a9f79d9cea26
steveoh <sgourley@utah.gov>
2021-12-07 20:33:00 -0700

    feat(app): create expandable rows of inventories
    refs #111

---
+++ src/api/uic-inventory/src/data/lookups.js
@@ -0,0 +1,23 @@
+export const wellTypes = [
+  {
+    value: -1,
+    label: 'General',
+  },
+  {
+    value: 5047,
+    label: 'Storm water drainage wells',
+  },
+  {
+    value: 5002,
+    label: 'Subsurface environmental remediation wells',
+  },
+  {
+    value: 5101,
+    label: 'UIC - Regulated large underground wastewater disposal system',
+    extra: '(LUWDS) => 5000 gdp',
+  },
+  {
+    value: 5026,
+    label: 'Veterinary, kennel, or pet grooming wastewater disposal system',
+  },
+];
