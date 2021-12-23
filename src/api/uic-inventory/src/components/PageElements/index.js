90c10132c6749df8610e65e26b1af813b1e1d4a6
steveoh <sgourley@utah.gov>
2021-12-23 10:41:06 -0700

    refactor(app): move confirmation modal to own component

--- src/api/uic-inventory/src/components/PageElements/index.js
+++ src/api/uic-inventory/src/components/PageElements/index.js
@@ -5,10 +5,11 @@ import onRequestError from './ToastErrors';
 import IncompleteSiteWarning from './IncompleteSiteWarning';
 import BackButton from './BackButton';
 import Tooltip from './Tooltip';
+import ConfirmationModal from './ConfirmationModal';
 
 export { ToastContainer } from 'react-toastify';
 export { Link, Route, BrowserRouter as Router, Switch, useParams, useHistory } from 'react-router-dom';
 export { toast } from 'react-toastify';
 export * from './Icons';
 
-export { BackButton, Chrome, Header, Navigation, onRequestError, IncompleteSiteWarning, Tooltip };
+export { BackButton, ConfirmationModal, Chrome, Header, Navigation, onRequestError, IncompleteSiteWarning, Tooltip };
