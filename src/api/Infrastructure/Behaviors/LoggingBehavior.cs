911b4129bc437328221598454629c18a3787a0a7
steveoh <sgourley@utah.gov>
2022-01-10 10:29:02 -0700

    chore(api): :evergreen_tree:

--- src/api/Infrastructure/Behaviors/LoggingBehavior.cs
+++ src/api/Infrastructure/Behaviors/LoggingBehavior.cs
@@ -4,7 +4,8 @@ using MediatR;
 using Serilog;
 
 namespace api.Infrastructure {
-  public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> {
+  public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse> {
     private readonly ILogger _logger;
 
     public LoggingBehavior(ILogger logger) {
