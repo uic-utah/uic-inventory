28f56936954f83ffa9d73d565734884ffbf53a99
steveoh <sgourley@utah.gov>
2022-01-12 17:42:22 -0700

    feat(api): create migration for ser contact flag

---
+++ src/api/Migrations/20220113004146_DenoteSerContacts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class DenoteSerContacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ser_contact",
+                schema: "public",
+                table: "contacts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ser_contact",
+                schema: "public",
+                table: "contacts");
+        }
+    }
+}
