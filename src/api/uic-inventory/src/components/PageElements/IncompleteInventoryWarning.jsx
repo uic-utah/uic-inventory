3ec989ad7bdbe3707c444e344d1a7735a20401ae
steveoh <sgourley@utah.gov>
2022-02-28 20:21:37 -0700

    chore(app): remove unused imports

--- src/api/uic-inventory/src/components/PageElements/IncompleteInventoryWarning.jsx
+++ src/api/uic-inventory/src/components/PageElements/IncompleteInventoryWarning.jsx
@@ -1,32 +1,32 @@
-import { CheckIcon, LocationMarkerIcon, UsersIcon, XIcon } from '@heroicons/react/solid';
+import { CheckIcon, XIcon } from '@heroicons/react/solid';
 import { Link } from '../PageElements';
 
 export default function IncompleteInventoryWarning({ siteId, inventoryId, inventoryStatus }) {
   return (
     <section>
-      <h2 className="mb-6 text-xl font-semibold text-center">This inventory is incomplete.</h2>
+      <h2 className="mb-6 text-center text-xl font-semibold">This inventory is incomplete.</h2>
       <h3 className="mb-2 text-lg font-medium">Inventory completeness includes</h3>
       <ul className="ml-8">
         <li>
-          <CheckIcon className="inline w-6 h-6 mr-4 stroke-current text-emerald-500" aria-label="yes" />
+          <CheckIcon className="mr-4 inline h-6 w-6 stroke-current text-emerald-500" aria-label="yes" />
           The inventory order number and type and
         </li>
         <li>
           {inventoryStatus?.locationStatus ? (
-            <CheckIcon className="inline w-6 h-6 mr-4 stroke-current text-emerald-500" aria-label="yes" />
+            <CheckIcon className="mr-4 inline h-6 w-6 stroke-current text-emerald-500" aria-label="yes" />
           ) : (
             <Link to={`/site/${siteId}/inventory/${inventoryId}/add-wells`}>
-              <XIcon className="inline w-6 h-6 mr-4 text-pink-500 stroke-current" aria-label="no" />
+              <XIcon className="mr-4 inline h-6 w-6 stroke-current text-pink-500" aria-label="no" />
             </Link>
           )}
           The inventory must have at least one well with a location and
         </li>
         <li>
           {inventoryStatus?.detailStatus ? (
-            <CheckIcon className="inline w-6 h-6 mr-4 stroke-current text-emerald-500" aria-label="yes" />
+            <CheckIcon className="mr-4 inline h-6 w-6 stroke-current text-emerald-500" aria-label="yes" />
           ) : (
             <Link to={`/site/${siteId}/inventory/${inventoryId}/add-well-details`}>
-              <XIcon className="inline w-6 h-6 mr-4 text-pink-500 stroke-current" aria-label="no" />
+              <XIcon className="mr-4 inline h-6 w-6 stroke-current text-pink-500" aria-label="no" />
             </Link>
           )}
           The well construction types, operating statuses, and well counts must have values{' '}
@@ -35,10 +35,10 @@ export default function IncompleteInventoryWarning({ siteId, inventoryId, invent
         {inventoryStatus?.subClass === 5002 && (
           <li>
             {inventoryStatus.contactStatus ? (
-              <CheckIcon className="inline w-6 h-6 mr-4 stroke-current text-emerald-500" aria-label="yes" />
+              <CheckIcon className="mr-4 inline h-6 w-6 stroke-current text-emerald-500" aria-label="yes" />
             ) : (
               <Link to={`/site/${siteId}/inventory/${inventoryId}/regulatory-contact`}>
-                <XIcon className="inline w-6 h-6 mr-4 text-pink-500 stroke-current" aria-label="no" />
+                <XIcon className="mr-4 inline h-6 w-6 stroke-current text-pink-500" aria-label="no" />
               </Link>
             )}
             A regulatory contact must be added.
