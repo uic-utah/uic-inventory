7fb03e59bf5bcd11a89a623932f9ba5c367e5cab
steveoh <sgourley@utah.gov>
2022-04-05 16:27:02 -0600

    chore(api): create migration to fix database enum

---
+++ src/api/Migrations/20220405213433_AccessLevel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations {
+  public partial class AccessLevel : Migration {
+    protected override void Up(MigrationBuilder migrationBuilder)
+      => migrationBuilder.Sql("ALTER TYPE public.\"access_level\" RENAME TO access_levels;");
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+      => migrationBuilder.Sql("ALTER TYPE public.\"access_levels\" RENAME TO access_level;");
+  }
+}
