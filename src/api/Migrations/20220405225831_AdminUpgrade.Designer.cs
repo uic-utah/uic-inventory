3ae8166bbf1a8b8a261549c813fc98f9b59e9293
steveoh <sgourley@utah.gov>
2022-04-05 17:01:47 -0600

    feat(api): add admin promotion notification type

--- src/api/Migrations/20220405225831_AdminUpgrade.Designer.cs
+++ src/api/Migrations/20220405225831_AdminUpgrade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using api.Features;
@@ -13,9 +14,10 @@ using api.Infrastructure;
 namespace api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220405225831_AdminUpgrade")]
+    partial class AdminUpgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
