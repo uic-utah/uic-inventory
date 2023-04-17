6c83ba9cfb460a0ef247c3d2fcebdd3790116cd2
steveoh <sgourley@utah.gov>
2023-04-17 13:03:13 -0600

    chore(api): switch to podman-compose

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,9 +1,9 @@
 set -e
 cd "$(dirname "$0")/.."
 
-docker compose down
+podman-compose down
 echo "starting databases"
-docker compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
+podman-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
 
 echo "authenticating with GCP"
 gcloud auth application-default login
