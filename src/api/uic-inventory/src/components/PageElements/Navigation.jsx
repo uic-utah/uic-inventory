a5c6d292e07f98847d0715646f4c386d8eea3d8c
steveoh <sgourley@utah.gov>
2023-05-09 17:36:49 -0600

    style(app): put profile menu on top of map

--- src/api/uic-inventory/src/components/PageElements/Navigation.jsx
+++ src/api/uic-inventory/src/components/PageElements/Navigation.jsx
@@ -170,7 +170,7 @@ function Navigation({ authenticationStatus }) {
                               >
                                 <Menu.Items
                                   static
-                                  className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                 >
                                   {profile.map((item) => (
                                     <Menu.Item key={item.key}>
