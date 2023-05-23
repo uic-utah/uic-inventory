a6b8baf4ebab155f510e1072373fe732cd506a4e
steveoh <sgourley@utah.gov>
2023-05-22 18:48:56 -0600

    chore(api): database migration for new notification types

---
+++ src/api/Migrations/20230523003626_InventoryAndSiteContactNotificationEnums.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations;
+/// <inheritdoc />
+public partial class InventoryAndSiteContactNotificationEnums : Migration {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:public.notification_types", "new_user_account_registration,admin_promotion,inventory_submission,facility_contact_modified,approved_well_status_edit,approved_inventory_well_addition,approved_site_contact_addition,approved_site_contact_deletion")
+            .OldAnnotation("Npgsql:Enum:public.notification_types", "new_user_account_registration,admin_promotion,inventory_submission,facility_contact_modified,approved_well_status_edit");
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.AlterDatabase()
+            .Annotation("Npgsql:Enum:public.notification_types", "new_user_account_registration,admin_promotion,inventory_submission,facility_contact_modified,approved_well_status_edit")
+            .OldAnnotation("Npgsql:Enum:public.notification_types", "new_user_account_registration,admin_promotion,inventory_submission,facility_contact_modified,approved_well_status_edit,approved_inventory_well_addition,approved_site_contact_addition,approved_site_contact_deletion");
+}
