69a9722dc6d6538fe3ab2405deb57452a2f09c71
steveoh <sgourley@utah.gov>
2023-11-01 17:21:01 -0600

    chore: docker is better

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,9 +1,9 @@
 set -e
 cd "$(dirname "$0")/.."
 
-podman-compose down
+docker-compose down
 echo "starting databases"
-podman-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
+docker-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach db cache
 
 echo "authenticating with GCP"
 gcloud auth application-default login
