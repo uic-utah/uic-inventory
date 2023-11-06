34251cb5b49c70961734aeab16bd5be67a188cbf
steveoh <sgourley@utah.gov>
2023-11-06 13:12:07 -0700

    chore(api): create database migration to add siteid
    refs #259

--- src/api/Migrations/20231106200438_SiteId.Designer.cs
+++ src/api/Migrations/20231106200438_SiteId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using api.Features;
@@ -13,9 +14,11 @@ using api.Infrastructure;
 namespace api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231106200438_SiteId")]
+    partial class SiteId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/api/Migrations/20231106200438_SiteId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations;
+
+/// <inheritdoc />
+public partial class SiteId : Migration {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.AddColumn<string>(
+            name: "site_id",
+            schema: "public",
+            table: "sites",
+            type: "text",
+            nullable: true);
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropColumn(
+            name: "site_id",
+            schema: "public",
+            table: "sites");
+}
