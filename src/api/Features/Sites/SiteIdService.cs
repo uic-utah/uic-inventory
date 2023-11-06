b3d07a8d8cb2c5e4f3aae9e7ae2173a9ff7f1b13
steveoh <sgourley@utah.gov>
2023-11-06 14:00:04 -0700

    refactor(api): remove unused usings

--- src/api/Features/Sites/SiteIdService.cs
+++ src/api/Features/Sites/SiteIdService.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
