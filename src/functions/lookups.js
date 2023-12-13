010c22b6275f52b2885a2e99ddba54dbecfa1b64
steveoh <sgourley@utah.gov>
2023-12-12 17:26:36 -0700

    feat(api): create function to handle pdf creation
    refs #273

---
+++ src/functions/lookups.js
@@ -0,0 +1,190 @@
+export const valueToLabel = (list = [], value) => {
+  const item = list.find((x) => x.value === value);
+
+  return item?.label ?? value;
+};
+
+export const contactTypes = [
+  {
+    value: 0,
+    label: "Owner/Operator",
+  },
+  {
+    value: 1,
+    label: "Owner",
+  },
+  {
+    value: 2,
+    label: "Operator",
+  },
+  {
+    value: 3,
+    label: "Facility Manager",
+  },
+  {
+    value: 4,
+    label: "Legal Representative",
+  },
+  {
+    value: 5,
+    label: "Official Representative",
+  },
+  {
+    value: 6,
+    label: "Contractor",
+  },
+  {
+    value: 7,
+    label: "DEQ Dist Eng/Project Manager",
+  },
+  {
+    value: 8,
+    label: "Local Health Department",
+  },
+  {
+    value: 9,
+    label: "Permit Writer",
+  },
+  {
+    value: 10,
+    label: "Developer",
+  },
+  {
+    value: 11,
+    label: "Other",
+  },
+];
+
+export const serContactTypes = [
+  {
+    value: "project_manager",
+    label: "DEQ Dist Eng/Project Manager",
+  },
+];
+
+export const serDivisions = [
+  { value: "Division of Air Quality" },
+  { value: "Division of Drinking Water" },
+  { value: "Division of Environmental Response and Remediation" },
+  { value: "Division of Waste Management and Radiation Control" },
+  { value: "Division of Water Quality" },
+];
+
+export const ownershipTypes = [
+  {
+    value: "PB",
+    label: "Private, For-Profit",
+  },
+  {
+    value: "PN",
+    label: "Private, Not-For-Profit",
+  },
+  {
+    value: "PF",
+    label: "Private, Farm",
+  },
+  {
+    value: "PV",
+    label: "Private, Other",
+  },
+  {
+    value: "FG",
+    label: "Federal Government",
+  },
+  {
+    value: "SG",
+    label: "State Government",
+  },
+  {
+    value: "LG",
+    label: "Local Government",
+  },
+  {
+    value: "OT",
+    label: "Tribal Government",
+  },
+  {
+    value: "OI",
+    label: "Individual/Household",
+  },
+  {
+    value: "OR",
+    label: "Other",
+  },
+];
+
+export const operatingStatusTypes = [
+  { value: "AC", label: "Active" },
+  { value: "PA", label: "Abandoned - Approved" },
+  { value: "PR", label: "Proposed Under Authorization By Rule" },
+  { value: "OT", label: "Other" },
+];
+
+export const remediationTypes = [
+  { value: 1, label: "Brownfield" },
+  { value: 2, label: "LUST" },
+  { value: 3, label: "NPL" },
+  { value: 4, label: "RCRA" },
+  { value: 5, label: "Superfund" },
+  { value: 6, label: "TRI" },
+  { value: 7, label: "VCP" },
+  { value: 8, label: "DSHW" },
+  { value: 999, label: "Other" },
+];
+
+export const wellTypes = [
+  { value: -1, label: "General", primary: true, epa: "NO CODE" },
+  { value: 5000, label: "Agricultural Drainage", epa: "5H2" },
+  { value: 5001, label: "Aquifer Recharge", epa: "5B1" },
+  { value: 5002, label: "Subsurface environmental remediation wells", epa: "5B6", primary: true },
+  { value: 5003, label: "Direct Heat Re-Injection", epa: "5C3" },
+  { value: 5004, label: "Domestic Wastewater Treatment Plant Effluent Disposal", epa: "5D" },
+  { value: 5005, label: "Electric Power Re-Injection", epa: "5C4" },
+  { value: 5006, label: "Experimental Technology", epa: "5G" },
+  { value: 5007, label: "Ground Water Aquaculture Return Flow", epa: "5C5" },
+  { value: 5008, label: "Heat Pump/AC Return Flow", epa: "5C2" },
+  { value: 5010, label: "Industrial Drainage", epa: "5A" },
+  { value: 5011, label: "Industrial Process - Water and Waste (General)", epa: "5A24" },
+  { value: 5012, label: "Industrial Process - Car Wash", epa: "5A1" },
+  { value: 5013, label: "Industrial Process - Car Wash (no engine/undercarriage)", epa: "5A2" },
+  { value: 5014, label: "Industrial Process - Appliance Service", epa: "5A3" },
+  { value: 5015, label: "Industrial Process - Beauty/Barber", epa: "5A4" },
+  { value: 5016, label: "Industrial Process - Nail Salon", epa: "5A5" },
+  { value: 5017, label: "Industrial Process - Dry Cleaner", epa: "5A6" },
+  { value: 5018, label: "Industrial Process - Laundromat (no DC)", epa: "5A7" },
+  { value: 5019, label: "Industrial Process - Funeral Services", epa: "5A8" },
+  { value: 5020, label: "Industrial Process - Wood/Furniture Finishing", epa: "5A9" },
+  { value: 5021, label: "Industrial Process - Machine/Welding", epa: "5A10" },
+  { value: 5022, label: "Industrial Process - Medical Services", epa: "5A11" },
+  { value: 5023, label: "Industrial Process - Pesticide Services", epa: "5A12" },
+  { value: 5024, label: "Industrial Process - Photographic Processing", epa: "5A13" },
+  { value: 5025, label: "Industrial Process - Printing", epa: "5A14" },
+  { value: 5026, label: "Veterinary, kennel, or pet grooming wastewater disposal system", epa: "5A15", primary: true },
+  { value: 5027, label: "Industrial Process - Metal Plating", epa: "5A16" },
+  { value: 5028, label: "Industrial Process - Equipment Manufacturing", epa: "5A17" },
+  { value: 5029, label: "Industrial Process - Cooling Water (no additives)", epa: "5A18" },
+  { value: 5030, label: "Industrial Process - Cooling Water (with additives)", epa: "5A19" },
+  { value: 5031, label: "Industrial Process - Food Processing", epa: "5A20" },
+  { value: 5032, label: "Industrial Process - Small Engine", epa: "5A21" },
+  { value: 5034, label: "Industrial Process - Drinking Water Treatment Residual", epa: "5A23" },
+  { value: 5035, label: "In-Situ Fossil Fuel Recovery", epa: "5L2" },
+  { value: 5037, label: "Mining Sand Or Other Backfill", epa: "5I" },
+  { value: 5038, label: "Motor Vehicle Waste Disposal (BANNED)", epa: "5K" },
+  { value: 5040, label: "Saline Water Intrusion Barrier", epa: "5B2" },
+  {
+    value: 5101,
+    label: "UIC-Regulated large underground wastewater disposal system",
+    epa: "5F",
+    extra: "(LUWDS) => 5000 gdp",
+    primary: true,
+  },
+  { value: 5044, label: "Solution Mining", epa: "5L1" },
+  { value: 5045, label: "Other Drainage", epa: "5H3" },
+  { value: 5046, label: "Spent-Brine Return Flow", epa: "5C1" },
+  { value: 5047, label: "Storm water drainage wells", epa: "5H1", primary: true },
+  { value: 5048, label: "Subsidence Control Wells", epa: "5B3" },
+  { value: 5050, label: "Aquifer Storage and Recovery", epa: "5B4" },
+  { value: 5100, label: "CessPools/Untreated Sewage Waste Disposal", epa: "5E" },
+  { value: 5998, label: "Experimental Technology CO2 Sequestration", epa: "5G2" },
+  { value: 5999, label: "Other (Class V)", epa: "5X" },
+];
---
+++ src/functions/printService.js
@@ -0,0 +1,215 @@
+import ky from "ky";
+import * as geometryEngine from "@arcgis/core/geometry/geometryEngine.js";
+
+import Polygon from "@arcgis/core/geometry/Polygon.js";
+import Point from "@arcgis/core/geometry/Point.js";
+
+const headers = {
+  Referer: "https://www.arcgis.com/apps/mapviewer/index.html?webmap=80c26c2104694bbab7408a4db4ed3382",
+};
+const timeout = 60000;
+
+export const getPrintMapImageAsync = async (site, wells = []) => {
+  const body = new URLSearchParams();
+  const outputSize = [850, 1200];
+
+  body.append(
+    "Web_Map_as_JSON",
+    JSON.stringify({
+      operationalLayers: [
+        {
+          type: "wmts",
+          customLayerParameters: null,
+          customParameters: null,
+          format: "image/png",
+          layer: "terrain_basemap",
+          style: "default",
+          tileMatrixSet: "0to19",
+          url: "https://discover.agrc.utah.gov/login/path/sector-snake-paradox-reply/wmts",
+          id: "WebTiled_9936",
+          title: "Utah Terrain Basemap (WMTS)",
+          opacity: 1,
+          minScale: 591658710.9094299,
+          maxScale: 1128.4994333389607,
+        },
+        {
+          id: "179a0f89200-layer-1",
+          title: "Utah Statewide Parcels",
+          opacity: 1,
+          minScale: 10489.342087,
+          maxScale: 0,
+          url: "https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/UtahStatewideParcels/FeatureServer/0",
+          layerType: "ArcGISFeatureLayer",
+          layerDefinition: {
+            definitionExpression: "",
+            drawingInfo: {
+              labelingInfo: [
+                {
+                  labelExpression: "[PARCEL_ID]",
+                  labelExpressionInfo: {
+                    expression: '$feature["PARCEL_ID"]',
+                  },
+                  labelPlacement: "esriServerPolygonPlacementAlwaysHorizontal",
+                  labelPosition: "curved",
+                  maxScale: 0,
+                  minScale: 1392.060877,
+                  repeatLabel: true,
+                  symbol: {
+                    type: "esriTS",
+                    color: [105, 105, 105, 255],
+                    font: {
+                      family: "Arial",
+                      size: 9.75,
+                    },
+                    horizontalAlignment: "center",
+                    kerning: true,
+                    haloColor: [211, 211, 211, 255],
+                    haloSize: 1,
+                    rotated: false,
+                    text: "",
+                    verticalAlignment: "baseline",
+                    xoffset: 0,
+                    yoffset: 0,
+                    angle: 0,
+                  },
+                },
+              ],
+              renderer: {
+                type: "simple",
+                symbol: {
+                  type: "esriSFS",
+                  color: null,
+                  outline: {
+                    type: "esriSLS",
+                    color: [224, 59, 153, 217],
+                    width: 0.93,
+                    style: "esriSLSSolid",
+                  },
+                  style: "esriSFSSolid",
+                },
+              },
+            },
+          },
+          showLabels: true,
+        },
+        {
+          featureCollection: {
+            layers: [
+              {
+                layerDefinition: {
+                  name: "Sites",
+                  geometryType: "esriGeometryPolygon",
+                },
+                featureSet: {
+                  geometryType: "esriGeometryPolygon",
+                  features: [site],
+                },
+              },
+            ],
+          },
+          id: "Sites",
+          title: "Sites",
+          opacity: 1,
+          minScale: 0,
+          maxScale: 0,
+        },
+        {
+          featureCollection: {
+            layers: [
+              {
+                layerDefinition: {
+                  name: "Wells",
+                  geometryType: "esriGeometryPoint",
+                },
+                featureSet: {
+                  geometryType: "esriGeometryPoint",
+                  features: wells,
+                },
+              },
+            ],
+          },
+          id: "Wells",
+          title: "Wells",
+          opacity: 1,
+          minScale: 0,
+          maxScale: 0,
+        },
+      ],
+      mapOptions: {
+        extent: getExtent([
+          new Polygon({
+            type: "polygon",
+            ...site.geometry,
+          }),
+          ...wells.map(
+            (well) =>
+              new Point({
+                type: "point",
+                ...well.geometry,
+              })
+          ),
+        ]),
+        spatialReference: {
+          wkid: 102100,
+        },
+        showAttribution: true,
+      },
+      exportOptions: {
+        dpi: 96,
+        outputSize,
+      },
+      layoutOptions: {
+        titleText: "UIC Inventory",
+        authorText: "",
+        copyrightText: "",
+        customTextElements: [],
+        scaleBarOptions: {
+          metricUnit: null,
+          nonMetricUnit: null,
+        },
+        legendOptions: {
+          operationalLayers: [],
+        },
+      },
+    })
+  );
+  body.append("Format", "JPG");
+  body.append("Layout_Template", "MAP_ONLY");
+  body.append("f", "json");
+
+  console.log("map request started");
+
+  const printJob = await ky
+    .post(
+      "https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task/execute",
+      { headers, body, timeout }
+    )
+    .json();
+
+  const imageUrl = printJob.results[0].value.url;
+
+  console.log("image location", imageUrl);
+
+  const image = await ky(imageUrl, {
+    headers,
+  }).arrayBuffer();
+
+  return `data:image/jpeg;base64,${Buffer.from(image).toString("base64")}`;
+};
+
+const getExtent = (geometries) => {
+  const [hull] = geometryEngine.convexHull(geometries, true);
+  const extent = hull.extent;
+  const area = extent.width + extent.height / 2;
+  let factor = 2;
+
+  if (area < 100) {
+    factor = 5;
+  } else if (area < 1000) {
+    factor = 4;
+  } else if (area < 10000) {
+    factor = 3;
+  }
+
+  return extent.expand(factor);
+};
