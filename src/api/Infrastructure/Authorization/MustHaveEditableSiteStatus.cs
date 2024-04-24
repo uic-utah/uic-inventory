6dcdc30fbf888aa4f491ef1d4407d81fb1be5f50
steveoh <sgourley@utah.gov>
2024-04-24 10:09:10 -0600

    fix(api): implement editable site status
    refs #143

--- src/api/Infrastructure/Authorization/MustHaveEditableSiteStatus.cs
+++ src/api/Infrastructure/Authorization/MustHaveEditableSiteStatus.cs
@@ -1,19 +1,34 @@
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
+using api.Features;
 using MediatR.Behaviors.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace api.Infrastructure;
 public class MustHaveEditableSiteStatus : IAuthorizationRequirement {
-    private class Handler(HasRequestMetadata metadata, ILogger log) : IAuthorizationHandler<MustHaveEditableSiteStatus> {
+    private class Handler(AppDbContext context, HasRequestMetadata metadata, ILogger log) : IAuthorizationHandler<MustHaveEditableSiteStatus> {
         private readonly ILogger _log = log;
         private readonly HasRequestMetadata _metadata = metadata;
+        private readonly AppDbContext _context = context;
+        private readonly InventoryStatus[] _draftInventoryStatus = [InventoryStatus.Incomplete, InventoryStatus.Complete];
 
-        public Task<AuthorizationResult> Handle(
+        public async Task<AuthorizationResult> Handle(
           MustHaveEditableSiteStatus requirement,
-          CancellationToken token = default) =>
-            Task.FromResult(AuthorizationResult.Succeed());
-        //! TODO: implement
-        // Task.FromResult(AuthorizationResult.Fail("S05:This site has been approved and can no longer be edited."));
+          CancellationToken token = default) {
+
+            var site = await _context.Sites
+            .Include(x => x.Inventories)
+            .SingleAsync(x => x.Id == _metadata.Site.Id, token);
+
+
+            if (site.Inventories.All(x => _draftInventoryStatus.Contains(x.Status))) {
+                return AuthorizationResult.Succeed();
+            }
+
+            return AuthorizationResult.Fail("S03:You cannot make changes to a site with approved inventories.");
+        }
     }
 }
