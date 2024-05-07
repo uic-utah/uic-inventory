c309467518673a2a7327a4c833d57b68b73f1196
steveoh <sgourley@utah.gov>
2024-05-06 19:34:56 -0600

    style(api): format

--- src/api/Features/Contacts/ContactQueries.cs
+++ src/api/Features/Contacts/ContactQueries.cs
@@ -8,30 +8,30 @@ using Serilog;
 
 namespace api.Features;
 public static class GetSiteContacts {
-  public class Query(int siteId) : IRequest<SiteContactPayload> {
-    public int SiteId { get; } = siteId;
+    public class Query(int siteId) : IRequest<SiteContactPayload> {
+        public int SiteId { get; } = siteId;
 
-    public class Handler(
-      AppDbContext context,
-      HasRequestMetadata metadata,
-      ILogger log) : IRequestHandler<Query, SiteContactPayload> {
-      private readonly AppDbContext _context = context;
-      private readonly HasRequestMetadata _metadata = metadata;
-      private readonly ILogger _log = log;
+        public class Handler(
+          AppDbContext context,
+          HasRequestMetadata metadata,
+          ILogger log) : IRequestHandler<Query, SiteContactPayload> {
+            private readonly AppDbContext _context = context;
+            private readonly HasRequestMetadata _metadata = metadata;
+            private readonly ILogger _log = log;
 
-      public async Task<SiteContactPayload> Handle(Query request, CancellationToken cancellationToken) {
-        var site = await _context.Sites
-          .Include(x => x.Account)
-          .Include(x => x.Contacts)
-          .Where(site => site.Id == request.SiteId)
-          .FirstOrDefaultAsync(cancellationToken);
+            public async Task<SiteContactPayload> Handle(Query request, CancellationToken cancellationToken) {
+                var site = await _context.Sites
+                  .Include(x => x.Account)
+                  .Include(x => x.Contacts)
+                  .Where(site => site.Id == request.SiteId)
+                  .FirstOrDefaultAsync(cancellationToken);
 
-        var contacts = site?.Contacts
-          .Select(contact => new ContactPayload(contact))
-          .ToArray() ?? [];
+                var contacts = site?.Contacts
+                  .Select(contact => new ContactPayload(contact))
+                  .ToArray() ?? [];
 
-        return new SiteContactPayload(site ?? new(), contacts);
-      }
+                return new SiteContactPayload(site ?? new(), contacts);
+            }
+        }
     }
-  }
 }
