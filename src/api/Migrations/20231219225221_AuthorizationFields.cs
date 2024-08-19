fa3b696a7dbac84a39b355049712dde5e4e9ec79
steveoh <sgourley@utah.gov>
2024-08-19 16:15:38 -0600

    chore(api): formatting

--- src/api/Migrations/20231219225221_AuthorizationFields.cs
+++ src/api/Migrations/20231219225221_AuthorizationFields.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace api.Migrations; 
+namespace api.Migrations;
 /// <inheritdoc />
 public partial class AuthorizationFields : Migration {
     /// <inheritdoc />
